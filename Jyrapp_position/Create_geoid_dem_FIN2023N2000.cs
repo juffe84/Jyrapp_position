40b3754e9d0e9358d1b068b9e22407c9088d766b
Juha <e2000936@edu.vamk.fi>
2025-08-15 11:39:02 +0300

    added FIN2023N2000 geoid

---
+++ Jyrapp_position/Create_geoid_dem_FIN2023N2000.cs
@@ -0,0 +1,72 @@
+﻿namespace Jyrapp_position;
+
+public class Create_geoid_dem_FIN2023N2000
+{
+        private static dem_node[,] dem = new dem_node[1140, 650];
+
+    public static void create_dem()
+    {
+        int row = 1140;
+        int col = 1;
+        double lat;
+        double lon;
+        double height;
+        String[] input = File.ReadAllText("FIN2023N2000.lst").Split('\n');
+        //70.700000  17.480000  34.415
+        //70,00000000  10,04000000  41,5820
+        //lat = Double.Parse(input[i].Substring(0,11));
+        //lon = Double.Parse(input[i].Substring(13,11));
+        //height = Double.Parse(input[i].Substring(26));
+        //60.280000  24.160000  19.184 tama on FIN2005N00.lst formaatti
+        //"   67.000000   30.120000   17.566" tama on FIN2023N2000.lst formaatti
+
+        for (int i = 0; i < 741000; i++)
+        {
+            lat = Double.Parse(input[i].Substring(3, 9));
+            lon = Double.Parse(input[i].Substring(15, 9));
+            height = Double.Parse(input[i].Substring(27));
+            dem[row - 1, col - 1].lat = lat;
+            dem[row - 1, col - 1].lon = lon;
+            dem[row - 1, col - 1].height = height;
+            col++;
+
+            if (col > 650)
+            {
+                col = 1;
+                row--;
+            }
+        }
+    }
+
+    public static Geoid_Height get_dem(GPS_DD_Point gps)
+    {
+        int row;
+        int col;
+
+        double x, x1, x2, y, y1, y2;
+        double q11, q12, q21, q22;
+        double r1, r2, p;
+
+        row = (int) ((double) ((gps.LeveysasteDD - 58.8) / 0.01));
+        col = (int) ((double) ((gps.PituusasteDD - 19.0) / 0.02));
+
+        q12 = dem[row + 1, col].height;
+        q22 = dem[row + 1, col + 1].height;
+        q11 = dem[row, col].height;
+        q21 = dem[row, col + 1].height;
+
+        x = gps.PituusasteDD;
+        x1 = dem[row, col].lon;
+        x2 = dem[row, col + 1].lon;
+
+        y = gps.LeveysasteDD;
+        y1 = dem[row, col].lat;
+        y2 = dem[row + 1, col].lat;
+
+        r1 = ((x2 - x) / (x2 - x1)) * q11 + ((x - x1) / (x2 - x1)) * q21;
+        r2 = ((x2 - x) / (x2 - x1)) * q12 + ((x - x1) / (x2 - x1)) * q22;
+        p = ((y2 - y) / (y2 - y1)) * r1 + ((y - y1) / (y2 - y1)) * r2;
+        
+        return new Geoid_Height(p);
+    }
+}
--- Jyrapp_position/Program.cs
+++ Jyrapp_position/Program.cs
@@ -6,8 +6,9 @@ using Jyrapp_position;
 var culture = new CultureInfo("en-US");
 CultureInfo.DefaultThreadCurrentCulture = culture;
 CultureInfo.DefaultThreadCurrentUICulture = culture;
-
-GPS_DD_Point gps = new GPS_DD_Point(63.07003952, 21.65800692, 21,36.157);
+//Kuusamon suunta 66.825717, 28.987300
+GPS_DD_Point gps = new GPS_DD_Point(63.07003952, 21.65800692, 21,36.157); //vaasa
+//GPS_DD_Point gps = new GPS_DD_Point(67, 30, 21,36.157); //kuusamon suuntaa
 
 Coordinate_Conv_result utm = Coordinate_conv.to_UTM(gps);
 Console.WriteLine("Leveysaste: " + gps.LeveysasteDD);
@@ -18,9 +19,13 @@ Coordinate_Conv_result gk = Coordinate_conv.to_GK(gps);
 Console.WriteLine("N_GK21: " + gk.N);
 Console.WriteLine("E_GK21: " + gk.E);
 Create_geoid_dem.create_dem();
+Create_geoid_dem_FIN2023N2000.create_dem();
 Geoid_Height geoidikorkeus = Create_geoid_dem.get_dem(gps);
-Console.WriteLine("Geoidikorkeus: " + geoidikorkeus.Z);
-Console.WriteLine("Korkeus N2000 järjestelmässä: "+(gps.EllipsoidHeight-geoidikorkeus.Z));
+Geoid_Height geoidikorkeusFIN2023N2000 = Create_geoid_dem_FIN2023N2000.get_dem(gps);
+Console.WriteLine("Geoidikorkeus FIN2005N00 (N2000 vanha): " + geoidikorkeus.Z);
+Console.WriteLine("Geoidikorkeus FIN2023N2000 (N2000 uusi): " + geoidikorkeusFIN2023N2000.Z);
+Console.WriteLine("Korkeus N2000(vanha) järjestelmässä: "+(gps.EllipsoidHeight-geoidikorkeus.Z));
+Console.WriteLine("Korkeus N2000(uusi) järjestelmässä: "+(gps.EllipsoidHeight-geoidikorkeusFIN2023N2000.Z));
 
 ParseFaces.To_list("koti.mm.xml");
 //Kaatuu, jos piste on mallin ulkopuolella->tee virheilmoitus.
